f859dbb87fc88e93c19ca9bf1ce01d8820973a93
enveraguilo <enver_aguilo@hotmail.com>
2022-11-16 13:43:13 -0400

    InicialDB

---
+++ MyVet.Web/Data/Entities/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyVet.Web.Data.Entities
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+        public DbSet<Owner> Owners { get; set; }
+    }
+
+}
